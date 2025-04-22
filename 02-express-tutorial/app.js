30a166485996c6cc9f9bf8243b2d65ef59b6a7f7
Sayolight <sayolight@proton.me>
2025-04-22 17:45:22 +0200

    module 62

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,24 +1,19 @@
-const http = require("http");
-const { readFileSync } = require("fs");
+const express = require("express");
+const app = express();
 
-const homePage = readFileSync("./index.html");
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-
-  if (url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(homePage);
-    res.end();
-  } else if (url === "/about") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<h1>about page</h1>");
-    res.end();
-  } else {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    res.write("<h1>page not found</h1>");
-    res.end();
-  }
+app.get("/", (req, res) => {
+  console.log("user hit the resource");
+  res.status(200).send("Home Page");
 });
 
-server.listen(5000);
+app.get("/about", (req, res) => {
+  res.status(200).send("About Page");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>Resource not found</h1>");
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000...");
+});
