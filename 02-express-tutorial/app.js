d6b79b7cf77d818017ca7dc5f9ad3700922739d4
Sayolight <sayolight@proton.me>
2025-04-22 18:35:10 +0200

    module 79

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,8 +3,17 @@ const app = express();
 
 app.use(express.static("./methods-public"));
 
+app.use(express.urlencoded({ extended: false }));
 app.get("/api/people/", (req, res) => {
   res.status(200).json({ success: true, data: [] });
+});
+
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send("Please provide credentials");
 });
 
 app.listen(5000, () => {
