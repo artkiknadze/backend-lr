89b18d5ca369058b584d5053bbcd5d71cc6c2bf2
Sayolight <sayolight@proton.me>
2025-04-22 18:08:26 +0200

    module 70

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -25,6 +25,11 @@ app.get("/api/products/:productID", (req, res) => {
   return res.json(singleProduct);
 });
 
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+    console.log(req.params);
+    res.send("Hello World");
+});
+
 app.listen(5000, () => {
   console.log("Server is listening on port 5000...");
 });
