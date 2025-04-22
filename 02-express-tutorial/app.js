b4cc576c66792ab017a696df7edf4c438e639a7f
Sayolight <sayolight@proton.me>
2025-04-22 18:04:41 +0200

    module 69

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -13,6 +13,18 @@ app.get("/api/products", (req, res) => {
   res.json(newProducts);
 });
 
+app.get("/api/products/:productID", (req, res) => {
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("Product Does Not Exist");
+  }
+
+  return res.json(singleProduct);
+});
+
 app.listen(5000, () => {
   console.log("Server is listening on port 5000...");
 });
