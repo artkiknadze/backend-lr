1301bcb85a8f3814b122bf7167b859f29de5d9f0
Sayolight <sayolight@proton.me>
2025-04-22 17:34:39 +0200

    module 60

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,11 +1,14 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+const homePage = readFileSync("./index.html");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
 
   if (url === "/") {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<h1>home page</h1>");
+    res.write(homePage);
     res.end();
   } else if (url === "/about") {
     res.writeHead(200, { "Content-Type": "text/html" });
